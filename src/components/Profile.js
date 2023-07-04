232b59e6de5d1d4d9c296218a55ece0a937c4897
Egor Kirin <egorcom27@gmail.com>
2023-07-04 12:57:27 +0300

    модальное окно для редактирования профиля

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,23 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
 import AvatarPng from "../assets/img/avatar.png";
 import "../assets/style/Profile.css";
+import { user } from "../utils/objData";
+import ProfileModal from "./modal/ProfileModal";
 
 const Profile = () => {
+  const [showModal, setShowModal] = useState(false);
+
+  const handleShowModal = () => {
+    setShowModal(true);
+  };
+
   return (
     <>
       <div className="profile">
         <div className="profile__container">
           <img className="profile__img" src={AvatarPng} alt="Аватарка" />
           <div className="profile__info">
-            <div className="profile__name">Кирин Егор</div>
-            <div className="profile__nick">Zelelo</div>
+            <div className="profile__name">
+              {user.surname} {user.name}
+            </div>
+            <div className="profile__nick">{user.nickname}</div>
           </div>
         </div>
         <div className="profile__btns">
-            <button className="profile__btn profile__btn-grey">Редактировать профиль</button>
-            <button className="profile__btn profile__btn-red">Выйти</button>
+          <button
+            onClick={handleShowModal}
+            className="profile__btn profile__btn-grey"
+          >
+            Редактировать профиль
+          </button>
+          <button className="profile__btn profile__btn-red">Выйти</button>
         </div>
       </div>
+
+      <ProfileModal showModal={showModal} onHide={() => setShowModal(false)} />
     </>
   );
 };
