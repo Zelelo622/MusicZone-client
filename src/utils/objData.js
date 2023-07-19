dacdcf6ae8dbb8617d369551b7f5cf40ff410f94
Egor Kirin <egorcom27@gmail.com>
2023-07-19 19:56:21 +0300

    добавлены новые обложки

--- src/utils/objData.js
+++ src/utils/objData.js
@@ -1,5 +1,7 @@
-import PlaylistPng from "../assets/img/playlist-1.png";
-import AlbomPng from "../assets/img/album-cover.png";
+import PlaylistFavoritePng from "../assets/img/playlists/playlist-1.png";
+import PlaylistIndiRockJpg from "../assets/img/playlists/playlistIndi.jpg";
+import PlaylistRockJpg from "../assets/img/playlists/playlistRock.jpg";
+import AlbomKinoPng from "../assets/img/albom/album-cover.png";
 
 import track1 from "../assets/music/Завтра война.mp3";
 import track2 from "../assets/music/Дерево.mp3";
@@ -11,42 +13,21 @@ export const playlists = [
     name: "Любимое",
     description: "",
     nickname: "",
-    imgPath: PlaylistPng,
+    imgPath: PlaylistFavoritePng,
   },
   {
     id: 2,
-    name: "Рок-хиты",
+    name: "Лучшие песни русского рока",
     description: "",
     nickname: "",
-    imgPath: PlaylistPng,
-  },
-  {
-    id: 3,
-    name: "Поп-хиты",
-    description: "",
-    nickname: "",
-    imgPath: PlaylistPng,
+    imgPath: PlaylistRockJpg,
   },
   {
     id: 4,
     name: "Инди-хиты",
     description: "",
     nickname: "",
-    imgPath: PlaylistPng,
-  },
-  {
-    id: 5,
-    name: "Хип-хоп-хиты",
-    description: "",
-    nickname: "",
-    imgPath: PlaylistPng,
-  },
-  {
-    id: 6,
-    name: "Шансон-хиты",
-    description: "",
-    nickname: "",
-    imgPath: PlaylistPng,
+    imgPath: PlaylistIndiRockJpg,
   },
 ];
 
@@ -55,25 +36,25 @@ export const music = [
     id: 1,
     name: "Завтра война",
     artist: "Кино",
-    imgPath: AlbomPng,
+    imgPath: AlbomKinoPng,
     soundPath: track1,
-    playlists: [1],
+    playlists: [],
   },
   {
     id: 2,
     name: "Дерево",
     artist: "Кино",
-    imgPath: AlbomPng,
+    imgPath: AlbomKinoPng,
     soundPath: track2,
-    playlists: [1, 4],
+    playlists: [2],
   },
   {
     id: 3,
     name: "Спокойная ночь",
     artist: "Кино",
-    imgPath: AlbomPng,
+    imgPath: AlbomKinoPng,
     soundPath: track3,
-    playlists: [1, 2],
+    playlists: [2],
   },
 ];
 
